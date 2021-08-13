23ff64217d18178580feed8a74e419666f295b28
Fadil <rosunmungurudm@gmail.com>
2021-08-13 21:43:58 +0400

    sqlite seed

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 - Available Commands:
 - - `php bin/console seed:patients` for [mysql, kb_doctor]
 - Packages :
-- - https://packagist.org/packages/catfan/medoo (DB Handler)
+- - https://packagist.org/packages/catfan/medoo (DB Handler) 
+- - https://medoo.in/doc (Medoo Documentation)
 - - https://packagist.org/packages/mnapoli/silly (CLI)
 - - https://packagist.org/packages/vlucas/phpdotenv (.env)
+- - https://packagist.org/packages/fakerphp/faker (*NEW* Faker)
+- - https://fakerphp.github.io/ (*NEW* Faker Documentation)
--- src/Command/Doctors.php
+++ src/Command/Doctors.php
@@ -3,23 +3,30 @@
 namespace App\Command;
 
 use App\Corebase\Db as Database;
-use Medoo\Medoo;
+use Faker\Factory;
 
 class Doctors extends Database
 {
+    private const ENTRY = 100;
+    private const TABLE = 'patients';
+
     public static function populatePatients()
     {
         $database = parent::connection();
-        $database->insert('patients', [
-            'name' => 'zyqqqqqx',
-            'addr' => 'wwwww',
-            'contact' => 'eeeee',
-            'age' => 'rrrr',
-            'sex' => 'tttt',
-            'patient_id' => '67',
-            'passwd' => '999',
-            'code' => '89898998922200',
-            'isAdmin' => 2,
-        ]);
+        $faker = Factory::create();
+
+        $database->delete(self::TABLE, []);
+
+        for($i=0; $i<self::ENTRY; $i++):
+            $database->insert(self::TABLE, [
+                'name'      => $faker->name(),
+                'addr'      => $faker->city(),
+                'contact'   => $faker->email(),
+                'age'       => $faker->numberBetween(18, 80),
+                'sex'       => $faker->randomElement(['Male', 'Female']),
+                'patient_id'=>$faker->numerify('fx-####'),
+            ]);
+            echo "Inseting ID:" . $database->id() . "\n";
+        endfor;
     }
 }
